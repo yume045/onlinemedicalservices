2c6a7c72151fa739c5d595b9eed2beb158397e9f
Nanthawad <32787546+Nanthawad@users.noreply.github.com>
2018-11-12 17:38:52 +0700

    ddd

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -19,26 +19,6 @@
                         </div>
                     </div>
     </div>
-<<<<<<< HEAD
-<form action="">
-      <div class="container">
-  <div class="row">
-    <div class="col-sm">
-      <b-card  title="Card Title"
-                  img-src="https://picsum.photos/600/300/?image=25"
-                  img-alt="Image"
-                  img-top
-                  tag="article"
-                  style="max-width: 20rem;margin-left:0%;"
-                  class="mb-2" >
-                    <p class="card-text">
-                      Some quick example text to build on the card title and make up the bulk of the card's content.
-                    </p>
-              <b-button href="#" variant="primary">Go somewhere</b-button>
-        </b-card>
-    </div>
-  </div>
-=======
 <div>
   <br>
 </div>
@@ -70,7 +50,6 @@
       </div>
     </div>
   </form>
->>>>>>> 8c8d826236585f84c0c210d3994223b4f2d0c017
 </div>
 </template>
 
